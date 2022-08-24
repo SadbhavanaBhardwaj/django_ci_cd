c16896fd4eaf51dadcff675f3c24b8ebc185c3c1
sadbhavanabhardwaj <sadbhavanahk@gmail.com>
2022-08-24 23:06:38 +0000

    nginx sites-enabled

--- nginx.sh
+++ nginx.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/ngnix/sites-available/app
-chmod 710  /var/lib/jenkins/workspace/django-cicd/
-sudo ln -s /etc/ngnix/sites-available/app /etc/ngnix/sites-enabled
+sudo cp -rf app.conf /etc/nginx/sites-available/app
+chmod 710 /var/lib/jenkins/workspace/django-cicd
 
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echo "nginx has been started"
+echo "Nginx has been started"
 
 sudo systemctl status nginx
-
