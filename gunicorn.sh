54f90f89409186f70e61092d71f2fecfd0cc2670
SadbhavanaBhardwaj <34401897+SadbhavanaBhardwaj@users.noreply.github.com>
2022-08-25 16:25:02 +0530

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -14,8 +14,8 @@ echo "migration done"
 
 cd /var/lib/jenkins/workspace/django-cicd/
 
-sudo cp -rf gunicorn.socket /etc/systemd/system
-sudo cp -rf gunicorn.service /etc/systemd/system
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
 
 
 echo $PWD
@@ -27,6 +27,6 @@ sudo systemctl enable gunicorn
 
 echo "gunicorn has been started"
 
-sudo systemctl status gunicorn
 sudo systemctl restart gunicorn
 
+sudo systemctl status gunicorn
